4d181340192b0b1d6d009068cd03e24211f714cd
Israel-Beni <israelbeni71@gmail.com>
2021-02-15 22:42:41 +0530

    script.js update, (this is the first operational version of script.js

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
+//import jsdom from "jsdom";
 
+// Type.fit API info
+const url = 'https://type.fit/api/quotes';
+
+// Page elements
+//const label = jsdom.window.document.querySelector('#label');
+//const submit = jsdom.window.document.querySelector('#button');
+
+
+// Add AJAX function here:
+const getQuotes = async() => {
+    try {
+        const response = await fetch(url, {cache: 'no-cache'});
+        if (response.ok) {
+            console.log('Response from API: ', response);
+            const jsonResponse = await response.json();
+            console.log('JsonResponse: ', jsonResponse);
+            return jsonResponse;
+        }
+        throw new Error("Request to 'https://type.fit/api/quotes' has failed!");
+    } catch(error) {
+        console.log('Error: ', error);
+    }
+}
+
+const chooseRandomQuote = (jsonResponse) => {
+    const randomIndex = Math.floor(Math.random() * jsonResponse.length);
+    return [jsonResponse[randomIndex].text, jsonResponse[randomIndex].author];
+}
+
+const renderQuote = (randomQuote) => {
+    console.log(`"${randomQuote[0]}"`);
+    console.log(`by ${randomQuote[1]}`);
+    //const labelContent = `<h4> ${randomQuote[0]} </h4>
+    //                      <label> <strong> ${randomQuote[1]}  </strong> </label>`
+    //label.append(labelContent);
+}
+
+getQuotes()
+    .then(chooseRandomQuote)
+    .then(renderQuote);
+
+//console.log('hello');
+//getQuotes();
